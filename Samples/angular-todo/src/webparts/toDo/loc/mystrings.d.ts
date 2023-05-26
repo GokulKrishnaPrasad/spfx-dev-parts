a6c95413bf66ddb498751446c0cc4ccf111da42b
GokulKrishnaPrasad <gokool4@gmail.com>
2023-05-27 01:33:57 +0530

    Angular Todo

---
+++ Samples/angular-todo/src/webparts/toDo/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface IToDoWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'ToDoWebPartStrings' {
+  const strings: IToDoWebPartStrings;
+  export = strings;
+}
